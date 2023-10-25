51d2a0507e3bd6ad8fa4f5b77cd3c1ee27def369
kihhmaster <kihhmaster@yandex.ru>
2023-10-25 13:54:35 +0300

    start

---
+++ src/js/import/main.js
@@ -0,0 +1,26 @@
+jQuery(function () { 
+
+	var reviewsAll
+	$('.js-detailed-offer__slider').each(function(){
+		var slider=$(this)
+		var reviewsAll = new Swiper(slider[0], {
+			// watchOverflow: true,
+			// watchSlidesVisibility: true,
+			// watchSlidesProgress: true,
+			// preventInteractionOnTransition: true,
+			// loop: true,
+			slidesPerView: 1,
+			navigation: {
+				nextEl: slider.find('.swiper-button-next')[0],
+				prevEl: slider.find('.swiper-button-prev')[0]
+			},
+			pagination: {
+				el: slider.find('.swiper-pagination')[0],
+				type: 'bullets',
+				clickable: true
+			},
+		});
+
+	})
+
+});
