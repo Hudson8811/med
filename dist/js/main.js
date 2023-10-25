a7522c2141c1f15a1b4a11f15a99100488d9808a
kihhmaster <kihhmaster@yandex.ru>
2023-10-25 18:56:42 +0300

    products

--- dist/js/main.js
+++ dist/js/main.js
@@ -55,5 +55,29 @@ jQuery(function () {
 		});
 
 	})
+	var productsSlider
+	$('.js-products__slider').each(function(){
+		var slider=$(this)
+		var productsSlider = new Swiper(slider[0], {
+			// watchOverflow: true,
+			// watchSlidesVisibility: true,
+			// watchSlidesProgress: true,
+			// preventInteractionOnTransition: true,
+			loop: true,
+			centeredSlides: true,
+			spaceBetween: 25,
+			slidesPerView: 3,
+			navigation: {
+				nextEl: slider.find('.swiper-button-next')[0],
+				prevEl: slider.find('.swiper-button-prev')[0]
+			},
+			pagination: {
+				el: slider.find('.swiper-pagination')[0],
+				type: 'bullets',
+				clickable: true
+			},
+		});
+
+	})
 
 });
