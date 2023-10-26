1d6a6d6f8d699d154db1204357c3b4c728bfbbd6
kihhmaster <kihhmaster@yandex.ru>
2023-10-26 17:54:35 +0300

    Декстов готов

--- dist/js/main.js
+++ dist/js/main.js
@@ -79,5 +79,48 @@ jQuery(function () {
 		});
 
 	})
+	var customerSlider
+	$('.js-customer__slider').each(function(){
+		var slider=$(this)
+		var customerSlider = new Swiper(slider[0], {
+			// watchOverflow: true,
+			// watchSlidesVisibility: true,
+			// watchSlidesProgress: true,
+			// preventInteractionOnTransition: true,
+			// loop: true,
+			// centeredSlides: true,
+			spaceBetween: 25,
+			slidesPerView: 4,
+			navigation: {
+				nextEl: ".customer__swiper-button-next",
+				prevEl: ".customer__swiper-button-prev"
+			},
+			// pagination: {
+			// 	el: slider.find('.swiper-pagination')[0],
+			// 	type: 'bullets',
+			// 	clickable: true
+			// },
+		});
+
+	})
+	var partnersSlider
+	$('.js-partners__slider').each(function(){
+		var slider=$(this)
+		var partnersSlider = new Swiper(slider[0], {
+			spaceBetween: 30,
+			speed: 2000,
+			freeMode: true,
+			loop: true,
+			pagination: false,
+			// centeredSlides: true,
+			slidesPerView: 'auto',
+			// setWrapperSize: true,
+			autoplay: {
+				delay: 0,
+				// reverseDirection: true,
+			},	
+		});
+
+	})
 
 });
